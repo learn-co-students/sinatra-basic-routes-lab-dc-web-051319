cdb956c78e2ffb15b051c5a035d7edd3149538a5
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-02 00:38:26 -0400

    Completed lab

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    'My name is Nicholas Oroochena'
+  end
+
+  get '/hometown' do
+    'My hometown is Potomac, MD'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is Storytime'
+  end
 end
